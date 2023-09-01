3278a32f0d5ab9e114fa791d43e312c2482742ee
Santo Shakil <shakil.mezbah@gmail.com>
2023-09-01 13:01:39 +0600

    init

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use actix::{Actor, StreamHandler};
+use actix_web::{
+    get, middleware::Logger, post, web, App, Error, HttpRequest, HttpResponse, HttpServer,
+    Responder,
+};
+use actix_web_actors::ws;
+
+#[tokio::main]
+async fn main() {
+    let server = HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+            .route("/ws/", web::get().to(index))
+    })
+    .bind(("127.0.0.1", 8080));
+    if let Ok(server) = server {
+        _ = server.run().await;
+    }
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+struct MyWs;
+
+impl Actor for MyWs {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+
+async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(MyWs {}, &req, stream);
+    println!("{:?}", resp);
+    resp
+}
